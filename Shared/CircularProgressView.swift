eb8121d9476bf4a31f7a66634339a3367bc83eed
Tanguy-Supinfo <tanguy.joseph@supinfo.com>
2022-06-14 11:53:53 +0200

    Rounded bar user ECTS

---
+++ Shared/CircularProgressView.swift
@@ -0,0 +1,40 @@
+//
+//  CircularProgressView.swift
+//  Campus Booster
+//
+//  Created by m1 on 14/06/2022.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    let progress: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(
+                    Color.pink.opacity(0.5),
+                    lineWidth: 15
+                )
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(
+                    Color.pink,
+                    style: StrokeStyle(
+                        lineWidth: 15,
+                        lineCap: .round
+                    )
+                )
+                .rotationEffect(.degrees(-90))
+                .animation(.easeOut, value: progress)
+
+        }
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProgressView(progress: 0.0)
+    }
+}
--- Shared/UserView.swift
+++ Shared/UserView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct UserView: View {
+
+    @State var progress: Double = 0.6
+    
     var body: some View {
-        Text("User View")
+        VStack {
+            
+            ZStack {
+                CircularProgressView(progress: progress)
+                
+                Image(systemName: "person").resizable()
+                .frame(width: 35.0, height: 35.0)
+                
+            }.frame(width: 100, height: 100)
+
+        }
     }
+    
 }
 
 struct UserView_Previews: PreviewProvider {
